8875b9be7a8888930c5df5794438d3a66e6de036
João Vitor Da Silva Barros <129674540+JVitorBarros@users.noreply.github.com>
2023-10-20 11:37:54 -0300

    .

--- JavaScript/aula08 - Funções/exercicios-funcoes/ex02.js
+++ JavaScript/aula08 - Funções/exercicios-funcoes/ex02.js
@@ -1,10 +1,8 @@
 /*
-Escreva uma função em JavaScript que realize a conversão de uma temperatura fornecida em graus Fahrenheit (F) para Celsius (C).
-
-Dica: para converter uma temperatura em F para C, subtraia 32 da temperatura e multiplique o resultado por 0,5556 (ou 5/9).
+ realize a conversão de uma temperatura fornecida em graus Fahrenheit (F) para Celsius (C).
 */
 
-// C = 5/9 x (F-32)
+
 
 function conversaoTemp(temp){
     let tempCels = (5/9) * (temp - 32);
