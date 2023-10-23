cba6950e1d0ff18f047a74ac1f846c9874592153
João Vitor Da Silva Barros <129674540+JVitorBarros@users.noreply.github.com>
2023-10-23 09:24:02 -0300

    .

---
+++ JavaScript/aula08 - Funções/exercicios-funcoes/ex03.js
@@ -0,0 +1,8 @@
+//  retorne a área de um triângulo, a partir dos valores de base e altura fornecidos.
+
+function areaTriangulo(base=0, altura=0){
+    let area = base*altura/2;
+    return area;  
+}
+
+let res = areaTriangulo(10, 15);
