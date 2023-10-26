edaa4da7f4df2bb596a4619ab21e3ef3a7be7633
João Vitor Da Silva Barros <129674540+JVitorBarros@users.noreply.github.com>
2023-10-26 10:42:36 -0300

    Update funcoes06.js

--- JavaScript/aula08 - Funções/funcoes06.js
+++ JavaScript/aula08 - Funções/funcoes06.js
@@ -1,12 +1,13 @@
-const soma = (...valores) =>{ /   // 10, 5, 15
-    const somar = (val) =>{
-        res = 0;
-        for(let v of val){
-            res +=v
-        }
-        return res
-    }
-    return somar(valores);
-}
+"use strict"
 
+const soma = (...valores) =>{
+    const somar = (val) =>{
+        let res = 0
+        for(let v of val){
+            res += v    
+        }
+        return res    
+    }
+    return somar(valores)
+}
 console.log(soma(10, 5, 15))
