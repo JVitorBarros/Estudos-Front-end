8c5cc9dbc0c397aa40326ea3d1d2505789e8b295
João Vitor Da Silva Barros <129674540+JVitorBarros@users.noreply.github.com>
2023-12-04 15:06:47 -0300

    .

--- JavaScript/aula08 - Manipulando DOM/aula05 - praticando/praticando01.js
+++ JavaScript/aula08 - Manipulando DOM/aula05 - praticando/praticando01.js
@@ -1,12 +1,22 @@
 const caixa01 = document.querySelectorAll("#caixa01")
-const caixa02 = document.querySelectorAll("#caixa02")
-const botao = document.querySelectorAll("#botaoCopiar")
+const caixa02 = document.querySelector("#caixa02")
+const botao = document.querySelector("#botao01")
 const todosCursos = [...document.querySelectorAll(".curso")]
+
 
 todosCursos.map((elemento)=>{
     elemento.addEventListener("click", (evento)=>{
         const selecionar = evento.target
-        selecionar.classList.add("destaque")
-
+          selecionar.classList.toggle("selecionar")
     })
 })
+
+botao.addEventListener("click", ()=>{
+    const cursoSelecionado = [...document.querySelectorAll(".selecionar")]
+    cursoSelecionado.map((elemento)=>{
+        caixa02.appendChild(elemento)
+    })
+})
+
+
+
