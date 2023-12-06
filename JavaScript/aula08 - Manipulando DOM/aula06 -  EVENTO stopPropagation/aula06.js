60fe00dd455ef84415f5126ab85dc076df991bfc
João Vitor Da Silva Barros <129674540+JVitorBarros@users.noreply.github.com>
2023-12-06 15:00:38 -0300

    .

--- JavaScript/aula08 - Manipulando DOM/aula06 -  EVENTO stopPropagation/aula06.js
+++ JavaScript/aula08 - Manipulando DOM/aula06 -  EVENTO stopPropagation/aula06.js
@@ -1,2 +1,12 @@
 const caixa01 = document.querySelector("#caixa01")
+const cursos = [...document.querySelectorAll(".curso")]
 
+caixa01.addEventListener("click", ()=>{
+    alert("clicou")
+
+})
+cursos.map((elemento)=>{
+    elemento.addEventListener("click", (evento)=>{
+        evento.stopPropagation()
+    })
+})
