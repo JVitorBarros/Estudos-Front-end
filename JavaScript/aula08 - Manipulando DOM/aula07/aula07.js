f7d30d93fe8ecf19121891389fb7d0bfb81f2b89
João Vitor Da Silva Barros <129674540+JVitorBarros@users.noreply.github.com>
2023-12-15 08:23:36 -0300

    .

--- JavaScript/aula08 - Manipulando DOM/aula07/aula07.js
+++ JavaScript/aula08 - Manipulando DOM/aula07/aula07.js
@@ -1,3 +1,6 @@
 const caixa01 = document.querySelector("#caixa01")
-const cursos = [...document.querySelectorAll(".curso")]
+const btn_c = [...document.querySelectorAll(".curso")]
 
+console.log(caixa01.firstElementChild)  //Retorna o primeiro elemento da coleção 'caixa01' 
+console.log(caixa01.lastElementChild)   //Retorna o ultimo elemento da coleção 'caixa01'
+console.log(caixa01.children)           //Retorna todos os elementos 
