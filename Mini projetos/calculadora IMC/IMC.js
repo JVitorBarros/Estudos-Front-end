9985f2dbe1ea08756950e98e957385604faf3314
João Vitor Da Silva Barros <129674540+JVitorBarros@users.noreply.github.com>
2023-12-21 15:51:22 -0300

    .

--- Mini projetos/calculadora IMC/IMC.js
+++ Mini projetos/calculadora IMC/IMC.js
@@ -1,28 +1,38 @@
-let nome = document.querySelector('#nome')
-let altura = document.querySelector('#altura')
-let peso = document.querySelector('#peso')
-let resultado = document.querySelector('#resultado')
-const botao_calcular = document.querySelector('#calcular')
+
+const botao_calcular = document.querySelector('#calcular');
 
 botao_calcular.addEventListener("click",()=>{
-    const calculo = peso / (altura * altura)
-
-    if(calculo < 18.5){
-        resultado.innerHTML = `${nome.value}, seu IMC é ${calculo.value} e seu estado atual é Magreza`
-
-    }else if(calculo >= 18.5 || calculo <= 24.9){
-        resultado.innerHTML = `${nome.value}, seu IMC é ${calculo.value} e seu estado atual é Normal`
-
-    }else if(calculo >= 25 || calculo <= 29.9){
-        resultado.innerHTML = `${nome}, seu IMC é ${calculo} e seu estado atual é Sobrepeso`
+    const nome = document.querySelector('#nome').value;
+    const altura = document.querySelector('#altura').value;
+    const peso = document.querySelector('#peso').value;
+    const resultado = document.querySelector('#resultado')
     
-    }else if(calculo >= 30 || calculo <= 34.9){
-        resultado.innerHTML = `${nome}, seu IMC é ${calculo} e seu estado atual é Obesidade grau I`
+    const valorIMC = (peso / (altura * altura)).toFixed(2)
+
+    if(nome == '' || altura == '' || peso == ''){
+       alert('Preencha os campos que estão faltando ')
+    }
+    else if(valorIMC < 18.5){
+        resultado.textContent = `${nome}, seu IMC é ${valorIMC} e seu estado atual é Magreza`
+
+    }
+    else if(valorIMC >= 18.5 || valorIMC <= 24.9){
+        resultado.textContent = `${nome}, seu IMC é ${valorIMC} e seu estado atual é Normal`
+
+    }
+    else if(valorIMC >= 25 || valorIMC <= 29.9){
+        resultado.textContent = `${nome}, seu IMC é ${valorIMC} e seu estado atual é Sobrepeso`
     
-    }else if(calculo >= 35 || calculo <= 39.9){
-        resultado.innerHTML = `${nome}, seu IMC é ${calculo} e seu estado atual é Obesidade grau II`
+    }
+    else if(valorIMC >= 30 || valorIMC <= 34.9){
+        resultado.textContent = `${nome}, seu IMC é ${valorIMC} e seu estado atual é Obesidade grau I`
     
-    }else{
-        resultado.innerHTML = `${nome.value}, seu IMC é ${calculo.value} e seu estado atual é Obesidade grau III`
+    }
+    else if(valorIMC >= 35 || valorIMC <= 39.9){
+        resultado.textContent = `${nome}, seu IMC é ${valorIMC} e seu estado atual é Obesidade grau II`
+    
+    }
+    else{
+        resultado.textContent = `${nome}, seu IMC é ${valorIMC} e seu estado atual é Obesidade grau III`
     }
 })
