d31a569bc1921cbb3d01fdc9ecdf9a2177d8d4db
João Vitor Da Silva Barros <129674540+JVitorBarros@users.noreply.github.com>
2023-12-27 15:01:57 -0300

    .

--- Mini projetos/01 - Lâmpada/lampada.js
+++ Mini projetos/01 - Lâmpada/lampada.js
@@ -1,6 +1,31 @@
-const botaoLigar = document.querySelector("botao-ligar");
-const botaoDesligar = document.querySelector("botao-desligar");
+const botaoLigar = document.querySelector('#botao-ligar');
+const botaoDesligar = document.querySelector('#botao-desligar');
+const imagem = document.querySelector("#img-lampada");
+
+imagem.addEventListener("mouseenter", ()=>{
+})
+
+
+imagem.addEventListener("dblclick", ()=>{
+    imagem.src = "img/lampada-quebrada.jpg";
+})
 
 botaoLigar.addEventListener("click",()=>{
+    if(imagem.src.endsWith("img/lampada-quebrada.jpg")){
+        alert("Não foi possivel. Lâmpada quebrada")
+    }else{
+        imagem.src = "img/ligada.jpg";
+    }
     
 })
+botaoDesligar.addEventListener("click", ()=>{
+    if(imagem.src.endsWith("img/lampada-quebrada.jpg")){
+        alert("Não foi possivel. Lâmpada quebrada")
+    }else{
+        imagem.src = "img/lampada-apagada.jpg";
+    }
+    
+})
+
+
+
