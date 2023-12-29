4e187c87e11042481d3e4d6a21178fb8a9753889
João Vitor Da Silva Barros <129674540+JVitorBarros@users.noreply.github.com>
2023-12-29 14:07:48 -0300

    .

---
+++ Mini projetos/02 - Semáforo/semaforo.js
@@ -0,0 +1,20 @@
+const btn_vermelho = document.querySelector("#vermelho")
+const btn_amarelo = document.querySelector("#amarelo")
+const btn_verde = document.querySelector("#verde")
+const btn_auto = document.querySelector("#auto")
+const semaforo = document.querySelector("#semaforo")
+
+
+
+btn_vermelho.addEventListener("click", ()=>{
+    semaforo.src = "img/vermelho.png";
+})
+btn_amarelo.addEventListener("click", ()=>{
+    semaforo.src = "img/amarelo.png";
+})
+btn_verde.addEventListener("click", ()=>{
+    semaforo.src = "img/verde.png";
+})
+btn_vermelho.addEventListener("click", ()=>{
+    semaforo.src = "img/vermelho.png";
+})
