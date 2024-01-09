0ea9e49c95db4f97a3d53a026026aa75b7ed2cac
João Vitor Da Silva Barros <129674540+JVitorBarros@users.noreply.github.com>
2024-01-09 15:24:35 -0300

    .

---
+++ Mini projetos/03 - slideshow/slideshow.js
@@ -0,0 +1,16 @@
+const adicionandoImagens = () =>{
+    
+    const imagemSlide = document.getElementsByClassName('imagem-slide');
+    const imagem = document.createElement('img');
+
+    let caminhosDasImagens = [
+        'img/imagens01.jpeg',
+        'img/imagens02.jpeg',
+        'img/imagens03.jpeg',
+        'img/imagens04.jpeg',
+        'img/imagens05.jpeg'
+    ]
+}
+
+console.log(adicionandoImagens());
+
